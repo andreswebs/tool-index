f77691886216bda7a42df08cf355a3f3c1e80036
Andre Silva <andreswebs@pm.me>
2023-01-12 23:15:07 -0300

    update

--- src/tracer.ts
+++ src/tracer.ts
@@ -5,17 +5,12 @@ import {
   DiagConsoleLogger,
   DiagLogLevel,
   trace,
-  // Attributes,
-  // SpanKind,
 } from '@opentelemetry/api';
 
 import {
-  SimpleSpanProcessor,
   // BatchSpanProcessor,
-  // AlwaysOnSampler,
-  // Sampler,
-  // SamplingDecision,
   // ConsoleSpanExporter,
+  SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
 
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
@@ -26,52 +21,18 @@ import {
 } from '@opentelemetry/sdk-trace-node';
 
 import { Resource } from '@opentelemetry/resources';
-
-import {
-  SemanticResourceAttributes,
-  // SemanticAttributes,
-} from '@opentelemetry/semantic-conventions';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
 import {
   HttpInstrumentation,
   IgnoreIncomingRequestFunction,
 } from '@opentelemetry/instrumentation-http';
+
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { MongooseInstrumentation } from '@opentelemetry/instrumentation-mongoose';
 
 // https://github.com/open-telemetry/opentelemetry-js/tree/main/experimental/packages/exporter-trace-otlp-http
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-
-// type FilterFunction = (
-//   spanName: string,
-//   spanKind: SpanKind,
-//   attributes: Attributes
-// ) => boolean;
-
-// function filterSampler(filterFn: FilterFunction, parent: Sampler): Sampler {
-//   return {
-//     shouldSample(ctx, tid, spanName, spanKind, attr, links) {
-//       if (!filterFn(spanName, spanKind, attr)) {
-//         return { decision: SamplingDecision.NOT_RECORD };
-//       }
-//       return parent.shouldSample(ctx, tid, spanName, spanKind, attr, links);
-//     },
-//     toString() {
-//       return `FilterSampler(${parent.toString()})`;
-//     },
-//   };
-// }
-
-// function ignoreHealthCheck(
-//   _spanName: string,
-//   spanKind: SpanKind,
-//   attributes: Attributes
-// ) {
-//   return (
-//     spanKind !== SpanKind.SERVER ||
-//     attributes[SemanticAttributes.HTTP_ROUTE] !== '/health'
-//   );
-// }
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR);
 
@@ -79,25 +40,25 @@ const tracerConfig: NodeTracerConfig = {
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
   }),
-  // sampler: filterSampler(ignoreHealthCheck, new AlwaysOnSampler()),
 };
 
 const provider = new NodeTracerProvider(tracerConfig);
 // const exporter = new ConsoleSpanExporter();
 const exporter = new OTLPTraceExporter();
+// const processor = new BatchSpanProcessor(exporter);
 const processor = new SimpleSpanProcessor(exporter);
 
 provider.addSpanProcessor(processor);
 
-const ignoreHealthCheck: IgnoreIncomingRequestFunction =
-  function ignoreHealthCheck(req) {
-    return req.url ? req.url.includes('/health') : false;
-  };
+const ignorePaths: IgnoreIncomingRequestFunction = function ignorePaths(req) {
+  const regex = /^\/(health*|computeMetadata*)$/;
+  return !!req.url.match(regex);
+};
 
 registerInstrumentations({
   instrumentations: [
     new HttpInstrumentation({
-      ignoreIncomingRequestHook: ignoreHealthCheck,
+      ignoreIncomingRequestHook: ignorePaths,
     }),
     new ExpressInstrumentation(),
     new MongooseInstrumentation(),
